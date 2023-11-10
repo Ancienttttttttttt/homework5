3820b5c1354d2e92173e72a03a2239d213a9d83b
Ancienttttttttttt <pila33rus_RU@mail.ru>
2023-11-10 23:05:20 +0300

    123

---
+++ homework5/main.py
@@ -0,0 +1,24 @@
+#Zadacha 1
+# a = str(input())
+# reversed_a = a[::-1]
+# print(reversed_a)
+
+#Zadacha 2
+# a = str(input())
+# count = 0
+# glas = set("aeiou")
+# for i in a:
+#     if i in glas:
+#         count += 1
+# print("Количество гласных букв: ")
+# print(count)
+
+#Zadacha 3
+# a = str(input())
+# print(a == a[::-1])
+
+#Zadacha 4
+# a = str(input())
+# words = a.split()
+# num_words = len(words)
+# print(num_words)
